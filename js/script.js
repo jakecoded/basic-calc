aac17c845e5ef570dfca559e3e638ed4c1c1b94f
Oliver Jake Bajas <oliverjakepublic@gmail.com>
2015-07-17 17:03:08 +0800

    Calculator Logic fixed and some modifications

--- js/script.js
+++ js/script.js
@@ -5,23 +5,88 @@
 
 $(function(){
 
+	var operator = '';
+	var tempval = 0;
+	var readyForClearScreen = 0;
+
+	function assignOperator(operation){
+		if( $('#display').val() ){
+			tempval = $('#display').val(); // store current value temporarily for later usess
+			operator = operation;
+		}
+	}
+
+	function resetOperator(){
+		operator = '';
+	}
+
+	function resetTempval(){
+		tempval = 0;
+	}
+
+	function resetReadyForClearScreen(){
+		readyForClearScreen = 0;
+	}
+
+	function clearTextbox(){
+		if(readyForClearScreen > 0)
+		{
+			$('#display').val('');
+		}
+	}
+
 	function displayResult(){
-		var total = eval(document.getElementById('display').value);
-		$('#display').val(total);
+		var total = 0;
+		if( $('#display').val() && tempval ){
+			if( operator == '+' ){
+				total = parseFloat(tempval) + parseFloat(document.getElementById('display').value);
+				$('#display').val(total);
+			}else if( operator == '-' ){
+				total = parseFloat(tempval) - parseFloat(document.getElementById('display').value);
+				$('#display').val(total);
+			}else if( operator == '/' ){
+				total = parseFloat(tempval) / parseFloat(document.getElementById('display').value);
+				$('#display').val(total);
+			}else if( operator == '*' ){
+				total = parseFloat(tempval) * parseFloat(document.getElementById('display').value);
+				$('#display').val(total);
+			}
+		}
+		resetTempval();
+		resetOperator();
 	}
 	
 	//buttons
 	$('.calc-buttons > button').click(function(){
+
+		//dont clear textbox if has dot on end
+		if( document.getElementById('display').value.slice(-1) !== '.' ){
+			clearTextbox();
+		}
+		
+		resetReadyForClearScreen();
+
 		$('#display').val($('#display').val() + $(this).text());
 	});
 
-	//equal functionalities
-	$('.equals > button').click(function(){
-		displayResult();
+	//dot button
+	$('.dot').click(function(){
+		if(document.getElementById('display').value.indexOf('.') === -1)
+		{
+		  $('#display').val($('#display').val() + '.');
+		}
 	});
 
-	//Enter
-	$(document).keypress(function(e){
+	//BASIC OPERATIONS
+	$('.operators > button').click(function(){
+		readyForClearScreen++;
+		displayResult();
+		assignOperator($(this).text());
+	});
+	
+
+	//equal functionalities
+	$('.equals > button').click(function(){
 		displayResult();
 	});
 
@@ -35,6 +100,8 @@ $(function(){
 	//reset
 	$('.clearbox > button').click(function(){
 		$('#display').val('');
+		resetOperator();
+		resetTempval();
 	});
 
 	//square root
